a5928acc74027a067c5098e2b086e7f621e7424c
Andy Nonaka <AJNonaka@lbl.gov>
2020-10-05 14:48:27 -0700

    memory leak

--- exec/compressible/main_driver.cpp
+++ exec/compressible/main_driver.cpp
@@ -475,9 +475,11 @@ void main_driver(const char* argv)
 
         // timer
         Real ts1 = ParallelDescriptor::second();
-    
+
+        /*
         RK3step(cu, cup, cup2, cup3, prim, source, eta, zeta, kappa, chi, D, flux,
                 stochFlux, cornx, corny, cornz, visccorn, rancorn, geom, dx, dt);
+        */
 
         // timer
         Real ts2 = ParallelDescriptor::second() - ts1;
